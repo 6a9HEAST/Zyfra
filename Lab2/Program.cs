e655b6917c3e382d3195704f3fa8a88e3a82ad32
6a9HEAST <lyovalite@gmail.com>
2024-11-27 02:50:34 +0300

    lesson3

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -3,21 +3,21 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Регистрация сервисов
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Настройка DbContext
+
 var connectionString = builder.Configuration.GetConnectionString("PostgreSqlConnection");
 builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(connectionString));
 
-// Регистрация репозитория
+
 builder.Services.AddScoped<IRepository, Repository>();
 
 var app = builder.Build();
 
-// Включение Swagger
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
